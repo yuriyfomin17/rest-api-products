61b715092745f21e61b8e07a342a65ec59e7f2fe
yuriyfomin17 <fominyura2012@gmail.com>
2020-09-20 20:37:24 +0300

    Updated

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/user')
+const checkAuth = require('../middleware/check-auth')
 router.post('/signup', UserController.user_sign_up)
 
 router.post('/login', UserController.user_login)
 
-router.delete('/:userId', UserController.user_delete)
+router.delete('/:userId',checkAuth, UserController.user_delete)
 module.exports = router;
